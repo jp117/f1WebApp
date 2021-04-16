1786443137d77434acb27b05c3c22f9c346f2cbb
jp117 <john.paradise117@gmail.com>
2021-04-16 11:50:49 -0400

    Moved nextRace state into the nextRace component instead of passing it in as a prop

--- f1webapp/src/App.js
+++ f1webapp/src/App.js
@@ -4,32 +4,16 @@ import NextRaceCard from "./components/nextRaceCard/NextRaceCard";
 import { useState, useEffect } from "react";
 
 function App() {
-    const [nextRace, setNextRace] = useState();
     const [localTime, setLocalTime] = useState(new Date());
 
-    useEffect(() => {
-        fetch("http://ergast.com/api/f1/current/next.json")
-            .then((res) => res.json())
-            .then((result) => {
-                setNextRace(result);
-            });
-    }, []);
-    if (nextRace != undefined) {
-        return (
-            <div className='App'>
-                <div className='container'>
-                    <Navbar />
-                    <NextRaceCard nextRace={nextRace.MRData.RaceTable.Races[0]} localTime={localTime} />
-                </div>
+    return (
+        <div className='App'>
+            <div className='container'>
+                <Navbar />
+                <NextRaceCard localTime={localTime} />
             </div>
-        );
-    } else {
-        return (
-            <div className='App'>
-                <h1>Still loading</h1>
-            </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default App;
--- f1webapp/src/components/nextRaceCard/NextRaceCard.js
+++ f1webapp/src/components/nextRaceCard/NextRaceCard.js
@@ -1,20 +1,36 @@
 import React from "react";
 import "./NextRaceCard.css";
+import { useState, useEffect } from "react";
 
 function NextRaceCard(props) {
-    console.log(props.localTime.getTimezoneOffset());
-    let raceDate = new Date(props.nextRace.date + "T" + props.nextRace.time);
-    console.log(raceDate);
-    return (
-        <div className='nextRaceCard'>
-            <div className='container'>
-                <div className='location'>NEXT RACE: {props.nextRace.Circuit.Location.country}</div>
-                <div className='time'>
-                    <span className='localtime'>{raceDate.toLocaleString()} Your Time</span>
+    const [nextRace, setNextRace] = useState();
+
+    useEffect(() => {
+        fetch("http://ergast.com/api/f1/current/next.json")
+            .then((res) => res.json())
+            .then((result) => {
+                setNextRace(result.MRData.RaceTable.Races[0]);
+            });
+    }, []);
+    if (nextRace != undefined) {
+        let raceDate = new Date(nextRace.date + "T" + nextRace.time);
+        return (
+            <div className='nextRaceCard'>
+                <div className='container'>
+                    <div className='location'>NEXT RACE: {nextRace.Circuit.Location.country}</div>
+                    <div className='time'>
+                        <span className='localtime'>{raceDate.toLocaleString()} Your Time</span>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div className='nextRaceCard'>
+                <div className='container'>LOADING NEXT RACE DATA</div>
+            </div>
+        );
+    }
 }
 
 export default NextRaceCard;
