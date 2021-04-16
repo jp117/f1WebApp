a6eab1c0695dad74147c55badf66eb8c143a73e0
jp117 <john.paradise117@gmail.com>
2021-04-16 11:38:55 -0400

    next race functionally completed to show next race location and time of race in user's local time

--- f1webapp/src/components/nextRaceCard/NextRaceCard.js
+++ f1webapp/src/components/nextRaceCard/NextRaceCard.js
@@ -3,14 +3,14 @@ import "./NextRaceCard.css";
 
 function NextRaceCard(props) {
     console.log(props.localTime.getTimezoneOffset());
+    let raceDate = new Date(props.nextRace.date + "T" + props.nextRace.time);
+    console.log(raceDate);
     return (
         <div className='nextRaceCard'>
             <div className='container'>
-                {props.nextRace.Circuit.Location.country}
-                {props.nextRace.date}
+                <div className='location'>NEXT RACE: {props.nextRace.Circuit.Location.country}</div>
                 <div className='time'>
-                    <p className='UTCTime'>{props.nextRace.time.slice(0, -4)} UTC</p>
-                    <p className='localTime'>your time</p>
+                    <span className='localtime'>{raceDate.toLocaleString()} Your Time</span>
                 </div>
             </div>
         </div>
